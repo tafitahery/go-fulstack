e3e982070b2b65a99260bccce8da23d0117a15a9
Tafita Hery <tafitahery@gmail.com>
2022-08-18 20:08:55 +0300

    Modifier et supprimer les données

--- app.js
+++ app.js
@@ -51,4 +51,16 @@ app.get('/api/stuff', (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+    .catch((error) => res.status(400).json(error));
+});
+
 module.exports = app;
