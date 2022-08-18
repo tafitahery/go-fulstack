5f679e8dcdd53d751ca53e2c486da2a29911b77d
Tafita Hery <tafitahery@gmail.com>
2022-08-18 17:37:48 +0300

    Configurer votre base de données

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose
+  .connect(
+    'mongodb+srv://tafita:lebo@cluster0.55d4v.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoBD échouée !'));
 
 app.use(express.json());
 
