35bedca40b0648e629a2f46dcb8e10c3da59ea1b
Tafita Hery <tafitahery@gmail.com>
2022-08-28 14:04:17 +0300

    Creez des tokens d'authentification

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -25,23 +26,23 @@ exports.login = (req, res, next) => {
         return res
           .status(401)
           .json({ message: 'Paire login/mot de passe incorrecte' });
-      } else {
-        bcrypt
-          .compare(req.body.password, user.password)
-          .then((valid) => {
-            if (!valid) {
-              return res
-                .status(401)
-                .json({ message: 'Paire login/mot de passe incorrecte' });
-            } else {
-              res.status(200).json({
-                userId: user._id,
-                token: 'TOKEN',
-              });
-            }
-          })
-          .catch((error) => res.status(500).json({ error }));
       }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res
+              .status(401)
+              .json({ message: 'Paire login/mot de passe incorrecte' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', {
+              expiresIn: '24h',
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
